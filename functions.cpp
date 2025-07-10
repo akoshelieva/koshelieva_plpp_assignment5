4ab25d6d18638b65ae7bad17fbbf567255346f68
akoshelieva <akoshelieva@kse.org.ua>
2025-07-10 21:40:02 +0300

    create function

---
+++ functions.cpp
@@ -0,0 +1,6 @@
+#include "functions.h"
+#include <cmath>
+double pow(double base, double exp) { return std::pow(base, exp); }
+double abs(double x) { return std::abs(x); }
+double max(double a, double b) { return a > b ? a : b; }
+double min(double a, double b) { return a < b ? a : b; }
---
+++ functions.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+#include <cmath>
+double pow(double base , double exp);
+double abs(double x);
+double max(double a, double b);
+double min(double a, double b);
+
+#endif //FUNCTIONS_H
